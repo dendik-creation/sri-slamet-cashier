a21db1d07feba13d55183874fc6ab4caf4bb73e5
Dendi' Setiawan <setiawandendik0205@gmail.com>
2025-12-01 19:27:29 +0700

    [feat] implements PWA laravel for standalone web app

---
+++ config/laravelpwa.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    "name" => "Kasir Sri Slamet",
+    "short_name" => "Kasir",
+    "start_url" => "/",
+    "background_color" => "#ffffff",
+    "theme_color" => "#e5e5e5",
+    "display" => "standalone",
+    "orientation" => "portrait",
+    "status_bar" => "#e5e5e5",
+    "icons" => [
+        "512x512" => [
+            "path" => "/icon.png",
+            "purpose" => "any",
+        ],
+    ],
+    "shortcuts" => [],
+    "custom" => [],
+];
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,11 +5,12 @@
     <title>{{ config('app.name') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/png" href="{{ asset('icon.png') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.tsx'])
     @inertiaHead
+    @laravelPWA
 </head>
   <body>
     @inertia
